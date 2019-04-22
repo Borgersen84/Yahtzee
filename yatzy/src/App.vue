39fd25a331491d10eb0bb7215302b3a0254af04c
Öyvind <oybo84@gmail.com>
2019-04-22 19:34:33 +0200

    basic styling to gamepage

--- yatzy/src/App.vue
+++ yatzy/src/App.vue
@@ -14,7 +14,7 @@ export default {
 
 <style>
   :root {
-	background-color: rgb(181, 221, 233);
+	background-color: rgb(208, 235, 243);
 	font-size: 62.5%;
   overflow: hidden;
 }
@@ -45,5 +45,30 @@ export default {
 
 #play-button:hover {
   cursor: pointer;
+}
+
+#dice-button {
+  font-size: 2.5rem;
+  color: rgb(74, 36, 172);
+  background-color: rgb(255, 254, 254);
+  border: 0.1rem solid black;
+}
+
+#dice-button:hover {
+  cursor: pointer;
+}
+
+table, th, td {
+  border: 1px solid black;
+}
+
+.sidesheet {
+  font-size: 1.5rem;
+  margin-top: 2.2rem;
+  background-color: rgb(250, 235, 168);
+}
+
+.shit {
+  width: 100%;
 }
 </style>
--- yatzy/src/components/Game.vue
+++ yatzy/src/components/Game.vue
@@ -1,3 +1,83 @@
 <template>
-    <h1>Game</h1>
+    <div>
+        <div class="center">
+            <button id="dice-button">Roll Dices</button>
+            <div class="sidesheet">
+                <table class="shit">
+                    <tr>
+                        <th></th>
+                        <th>Score</th>
+                    </tr>
+                    <tr>
+                        <td>Ones</td>
+                        <td id="ones">0</td>
+                    </tr>
+                    <tr>
+                        <td>Twos</td>
+                        <td id="twos">0</td>
+                    </tr>
+                    <tr>
+                        <td>Threes</td>
+                        <td id="threes">0</td>
+                    </tr>
+                    <tr>
+                        <td>Fours</td>
+                        <td id="fours">0</td>
+                    </tr>
+                    <tr>
+                        <td>Fives</td>
+                        <td id="fives">0</td>
+                    </tr>
+                    <tr>
+                        <td>Sixes</td>
+                        <td id="sixes">0</td>
+                    </tr>
+                    <tr>
+                        <td>Upper Total</td>
+                        <td id="upper">0</td>
+                    </tr>
+                    <tr>
+                        <td>Bonus</td>
+                        <td id="bonus">0</td>
+                    </tr>
+                    <tr>
+                        <td>3 of a kind</td>
+                        <td id="three-of-a">0</td>
+                    </tr>
+                    <tr>
+                        <td>4 of a kind</td>
+                        <td id="four-of-a">0</td>
+                    </tr>
+                    <tr>
+                        <td>Full House</td>
+                        <td id="fullhouse">0</td>
+                    </tr>
+                    <tr>
+                        <td>Small Straight</td>
+                        <td id="smallstraight">0</td>
+                    </tr>
+                    <tr>
+                        <td>Large Straight</td>
+                        <td id="largestraight">0</td>
+                    </tr>
+                    <tr>
+                        <td>YAHTZEE</td>
+                        <td id="yahtzee">0</td>
+                    </tr>
+                    <tr>
+                        <td>Chance</td>
+                        <td id="chance">0</td>
+                    </tr>
+                    <tr>
+                        <td>Yahtzee Bonus</td>
+                        <td id="yahtzee-bonus">0</td>
+                    </tr>
+                    <tr>
+                        <td>Sum</td>
+                        <td id="sum">0</td>
+                    </tr>
+                </table>
+            </div>
+        </div>  
+    </div>
 </template>
