6cfd0f2d15904a169e8ecbfe0f7fa4742320bb61
Öyvind <oybo84@gmail.com>
2019-04-22 14:11:59 +0200

    Further styling on homepage.

--- yatzy/src/components/Home.vue
+++ yatzy/src/components/Home.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="center">
+    <div>
+        <img src="src\images\yahtzee.jpg">
+        <div class="center">
         <button id="play-button">Play Yahtzee</button>
+        </div>  
     </div>
 </template>
