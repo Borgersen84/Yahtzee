2dc559dcaba5e8381db92d53efd622254f0867b4
Öyvind <oybo84@gmail.com>
2019-04-24 14:24:45 +0200

    dices added to game

--- yatzy/src/components/Game.vue
+++ yatzy/src/components/Game.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <div class="center">
-            <button id="dice-button">Roll Dices</button>
+            <div class="dice">
+                <p id="dice-one">{{newNumbers[0]}}</p>
+                <p id="dice-two">{{newNumbers[1]}}</p>
+                <p id="dice-three">{{newNumbers[2]}}</p>
+                <p id="dice-four">{{newNumbers[3]}}</p>
+                <p id="dice-five">{{newNumbers[4]}}</p>
+            </div>
+            <button 
+            id="dice-button"
+            @click="rollDices"
+            >Roll Dices</button>
             <div class="scoreboard">
                 <table class="scoreboard-column">
                     <tr>
@@ -10,7 +20,7 @@
                     </tr>
                     <tr>
                         <td>Ones</td>
-                        <td id="ones">0</td>
+                        <td id="ones"></td>
                     </tr>
                     <tr>
                         <td>Twos</td>
@@ -81,3 +91,78 @@
         </div>  
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            isValid: false,
+            dices: [1, 4, 6, 3, 2],
+            newNumbers: []
+        }
+    },
+    methods: {
+        rollDices() {
+            this.newNumbers = [];
+            var i;
+            for(i = 0; i < this.dices.length; i++){
+                this.dices[i] = Math.floor(Math.random() * 6) + 1;
+                this.newNumbers += this.dices[i];
+            }
+            return this.newNumbers;
+        },
+        getResult() {
+            var result = this.rollDices();
+
+        }
+    },
+    computed: {
+        returnDiceOne() {
+            return this.dices[0];
+        },
+        returnDiceTwo() {
+            return this.dices[1];
+        },
+        returnDiceThree() {
+            return this.dices[2];
+        },
+        returnDiceFour() {
+            return this.dices[3];
+        },
+        returnDiceFive() {
+            return this.dices[4];
+        }
+    }
+}
+</script>
+
+<style>
+    * {
+        font-family: Helvetica, Arial, sans-serif;
+        text-align: center;
+    }
+
+    .dice {
+        display: flex;
+        border: 1px solid gray;
+        margin: 0 auto;
+        margin-bottom: 1rem;
+        background-color: rgb(250, 235, 168);
+    }
+
+    #dice-one,
+    #dice-two,
+    #dice-three,
+    #dice-four,
+    #dice-five {
+        height: 7rem;
+        width: 7rem;
+        padding: 2.2rem;
+        margin: 0 auto;
+        border: 1px solid gray;
+        border-radius: 1rem;  
+        font-size: 3rem;
+        background-color: white;
+    }
+</style>
+
