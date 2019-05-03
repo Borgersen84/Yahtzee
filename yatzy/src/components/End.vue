208995196fe4c9576eedff1b3b1dba93f86a21eb
Öyvind <oybo84@gmail.com>
2019-05-03 23:45:34 +0200

    Removal of unused files

--- yatzy/src/components/End.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>End</h1>
-</template>
--- yatzy/src/routes.js
+++ yatzy/src/routes.js
@@ -1,9 +1,7 @@
 import Home from './components/Home.vue';
 import Game from './components/Game.vue';
-import End from './components/End.vue';
 
 export const routes = [
     {path: '/', component: Home},
-    {path: '/game', component: Game},
-    {path: '/end', component: End}
+    {path: '/game', component: Game}
 ];
